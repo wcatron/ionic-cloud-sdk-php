74a4e5e6d2a28368a49c0922630c2d8a56ed0e9c
wcatron <wcatron@catrondevelopment.com>
2016-09-06 18:24:32 -0400

    Fix pagination so get users runs.

--- src/Helpers/Pagination.php
+++ src/Helpers/Pagination.php
@@ -5,8 +5,11 @@ class Pagination {
     var $page = 0;
     var $pageSize = 10;
     var $currentPage = 0;
-    var $currentSize = 0;
+    /**
+     * @var null|int Current size of the results. Null at beginning when there is no current size.
+     */
+    var $currentSize = null;
     function isEnd() {
-        return ($this->currentSize < $this->pageSize);
+        return ($this->currentSize === null) ? false : ($this->currentSize < $this->pageSize);
     }
 }
